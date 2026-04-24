443894517e1ba66bdf6997e3e25551fad864b1b7
Rutuja Raut <145963900+rutujaraut730@users.noreply.github.com>
2026-04-24 19:44:12 +0530

    ADD LeetCode #193- Delete Duplicate Emails

--- leetcode.sql
+++ leetcode.sql
@@ -94,7 +94,14 @@ FROM Customers
 LEFT JOIN Orders ON Customers.id = Orders.customersId
 WHERE Orders.cutomersId is null;
 
+-- LeetCode #193- Delete Duplicate Emails
+-- Difficulty: Easy
+-- Topics: DELETE, Join, AND
 
+DELETE p1 
+FROM Person p1, Person p2
+WHERE p1.email = p2.email 
+  AND p1.id > p2.id;
 
 
 
