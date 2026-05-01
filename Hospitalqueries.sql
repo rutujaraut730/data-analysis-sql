cdb0703009c59b965ac6dfea3c6011336b48e28b
Rutuja Raut <145963900+rutujaraut730@users.noreply.github.com>
2026-05-01 20:56:31 +0530

    Update Hospitalqueries.sql

--- Hospitalqueries.sql
+++ Hospitalqueries.sql
@@ -19,3 +19,33 @@ INSERT INTO patients VALUES (7, 'Meera', 36, 'Fever', 'Delhi', 'Dr. Patel', 2800
 INSERT INTO patients VALUES (8, 'Karan', 72, 'Diabetes', 'Mumbai', 'Dr. Mehta', 22000);
 INSERT INTO patients VALUES (9, 'Pooja', 29, 'Fever', 'Bangalore', 'Dr. Sharma', 3200);
 INSERT INTO patients VALUES (10, 'Vivek', 58, 'Heart Disease', 'Delhi', 'Dr. Patel', 48000);
+
+-- Q1. Show all patients from Mumbai.
+SELECT patient_id, name FROM patients WHERE city = 'Mumbai';
+
+-- Q2. Show name and bill of patients whose bill is more than 10000.
+SELECT name, bill FROM patients WHERE bill > 10000;
+
+-- Q3. Show total bill amount for each disease.
+SELECT SUM(bill), disease FROM patients GROUP BY disease;
+
+-- Q4. Show average age of patients per city.
+SELECT AVG(age), city FROM patients GROUP BY city;
+
+-- Q5. Show the highest bill for each doctor.
+SELECT MAX(bill), doctor FROM patients GROUP BY doctor;
+
+-- Q6. Show diseases where average bill is more than 20000.
+SELECT disease FROM patients GROUP BY disease HAVING AVG(bill) > 20000;
+
+-- Q7. Show all patients sorted by age oldest to youngest.
+SELECT * FROM patients ORDER BY age DESC;
+
+-- Q8. Show patients who have Fever AND are from Delhi.
+SELECT name FROM patients WHERE disease = 'Fever' AND city = 'Delhi';
+
+-- Q9. Show count of patients per doctor.
+SELECT COUNT(patient_id), doctor FROM patients GROUP BY doctor;
+
+-- Q10. Show doctors where total bill collected is more than 50000 AND they have more than 2 patients.
+SELECT doctor FROM patients GROUP BY doctor HAVING SUM(bill) > 50000 AND COUNT(patient_id) > 2;
